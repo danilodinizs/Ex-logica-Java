577e1e688f64146b9267a11e634e6f1963f54bf9
Danilo <danilodiniz93@yahoo.com>
2024-05-02 15:29:27 -0300

    ex 01

--- src/Ex01.java
+++ src/Ex01.java
@@ -6,9 +6,13 @@ public class Ex01 {
 
         int a, b, c;
 
+        System.out.print("Primeiro número");
         a = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Segundo número");
         b = sc.nextInt();
         sc.nextLine();
+        System.out.print("Terceiro número");
         c = sc.nextInt();
 
         if (a + b < c) {
